f7784d0d41a6ed2d22af0cbd731800584ed39cff
Jhonathan Miceli Landa <jhonathanmicelilanda@MacBook-Air-de-Jhonathan.local>
2022-07-07 21:49:36 -0500

    upd

--- routes/owner.py
+++ routes/owner.py
@@ -12,7 +12,6 @@ ownerRoute = APIRouter()
 def shopOwner(id: int):
     dataOwner = conn.execute(select(tableUser.c.id,tableUser.c.name,tableUser.c.lastname,tableUser.c.phone,tableUser.c.email)
     .select_from(tableUser.join(tableOwner).join(tableShop)).where(tableUser.c.id == id).filter(tableOwner.c.user_id == tableUser.c.id).filter(tableShop.c.owner_id == tableOwner.c.id)).first()
-    print(dataOwner,' -none?')
     if dataOwner == []:
         return {"Error":"No se encontro al dueño"}
     else:
