25b43a9154a879d169704a61e99a6bf0a45a83a1
Jhonathan Miceli Landa <jhonathanmicelilanda@MacBook-Air-de-Jhonathan.local>
2022-07-08 01:50:12 -0500

    try-catch

--- routes/owner.py
+++ routes/owner.py
@@ -10,34 +10,37 @@ ownerRoute = APIRouter()
 
 @ownerRoute.get("/owner/shop/{id}")
 def shopOwner(id: int):
+    try:
     # dataOwner = conn.execute(select(tableUser.c.id,tableUser.c.name,tableUser.c.lastname,tableUser.c.phone,tableUser.c.email)
     # .select_from(tableUser.join(tableOwner).join(tableShop)).where(tableUser.c.id == id).filter(tableOwner.c.user_id == tableUser.c.id).filter(tableShop.c.owner_id == tableOwner.c.id)).first()
-    dataOwner = conn.execute(select(tableUser.c.id,tableUser.c.name,tableUser.c.email,tableUser.c.phone,tableUser.c.isowner).select_from(tableUser).where(tableUser.c.id == id)).first()
-# select u.name, s.name from user u join owner o on u.id = o.user_id join shop s on s.owner_id = o.user_id;
-    if dataOwner == []:
-        return {"Error":"No se encontro al dueño"}
-    else:
-        dataShop = conn.execute(select(tableShop).select_from(tableShop).where(tableShop.c.owner_id == id)).first()
-        if dataShop == []:
-            return {"Error":"No es dueño"}
-        else: 
-            if dataShop == None and dataOwner.isowner == 1:
+        dataOwner = conn.execute(select(tableUser.c.id,tableUser.c.name,tableUser.c.email,tableUser.c.phone,tableUser.c.isowner).select_from(tableUser).where(tableUser.c.id == id)).first()
+    # select u.name, s.name from user u join owner o on u.id = o.user_id join shop s on s.owner_id = o.user_id;
+        if dataOwner == []:
+            return {"Error":"No se encontro al dueño"}
+        else:
+            dataShop = conn.execute(select(tableShop).select_from(tableShop.join(tableOwner,tableShop.c.owner_id == tableOwner.c.user_id)).where(tableOwner.c.user_id == id)).fetchall()
+            print(dataShop)
+            if dataShop == [] and dataOwner.isowner == 1:
                 return {"res":{"Owner":{"user":dataOwner,"shop":"No tiene tiendas registradas"}}}
-            elif dataShop == None or dataOwner == 0:
-                return {"Error":"No es dueño"}
-            else:
-                return {"res":{"Owner":{"user":dataOwner,"shop":dataShop}}}
+            else: 
+                if dataShop == None and dataOwner.isowner == 1:
+                    return {"res":{"Owner":{"user":dataOwner,"shop":"No tiene tiendas registradas"}}}
+                elif dataShop == [] or dataOwner == 0:
+                    return {"Error":"No es dueño"}
+                else:
+                    return {"res":{"Owner":{"user":dataOwner,"shop":dataShop}}}
+    except Exception as e:
+        return {"Error":str(e)}
 
 # ENVIAR EL ID DEL OWNER EN LA PETICION PARA CREAR EL RESTAURANTE PERTENECIENTE A ESE DUEÑO
 @ownerRoute.post("/owner/shop/create")
 def createShop(shop:Shop):
-
-    # dataOwner = conn.execute(select(tableUser.c.id,tableUser.c.name).select_from(tableUser).where(tableUser.c.id == id)).first()
-
-
-    test = conn.execute(select(tableUser.c.isowner).where(tableUser.c.id == shop.owner_id)).first()
-    if(test.isowner == 0):
-        return {"Error":"No es un dueño"}
-    else:
-        conn.execute(tableShop.insert(), shop.dict())
-    return {"Restaurant":'Creado correctamente'}
+    try:
+        test = conn.execute(select(tableUser.c.isowner).where(tableUser.c.id == shop.owner_id)).first()
+        if(test.isowner == 0):
+            return {"Error":"No es un dueño"}
+        else:
+            conn.execute(tableShop.insert(), shop.dict())
+        return {"Restaurant":'Creado correctamente'}
+    except Exception as e:
+        return {"Error":str(e)}
--- routes/user.py
+++ routes/user.py
@@ -23,64 +23,79 @@ userRoute = APIRouter()
 
 @userRoute.get("/")
 def getUsers():
+    try:
     # join table user with table owner
     #query = conn.execute(select(tableUser.c.id,tableUser.c.name,tableUser.c.lastname,tableUser.c.url_avatar,tableUser.c.email,tableUser.c.phone,tableUser.c.isowner,tableOwner).select_from(tableUser.join(tableOwner, tableUser.c.id == tableOwner.c.user_id))).fetchall()
-    user = conn.execute(tableUser.select()).fetchall()
-    # return {"Owner":query}
-    return user
-
+        user = conn.execute(tableUser.select()).fetchall()
+        # return {"Owner":query}
+        return user
+    except Exception as e:
+        return {"Error":str(e)}
+        
 @userRoute.delete("/auth/delete/{id}")
 def deleteUser(id: int):
-    type = conn.execute(tableUser.select().where(tableUser.c.id == id)).first()
-    if type.isowner == 1:
-        conn.execute(tableOwner.delete().where(tableOwner.c.user_id == id))
-        conn.execute(tableUser.delete().where(tableUser.c.id == id))
-    else:
-        conn.execute(tableUser.delete().where(tableUser.c.id == id))
-    return {"message": "Usuario eliminado exitosamente"}
-
+    try:
+        type = conn.execute(tableUser.select().where(tableUser.c.id == id)).first()
+        if type.isowner == 1:
+            conn.execute(tableOwner.delete().where(tableOwner.c.user_id == id))
+            conn.execute(tableUser.delete().where(tableUser.c.id == id))
+        else:
+            conn.execute(tableUser.delete().where(tableUser.c.id == id))
+        return {"message": "Usuario eliminado exitosamente"}
+    except Exception as e:
+        return {"Error":str(e)}
+        
 @userRoute.put("/auth/update/{id}")
 def updateUser(id: int, user: UsuarioUpdate):
-    conn.execute(tableUser.update().where(tableUser.c.id == id).values(name=user.name,lastname=user.lastname,url_avatar=user.url_avatar,email=user.email,phone=user.phone))
-    return {"message": "Usuario actualizado exitosamente"}
+    try:
+        conn.execute(tableUser.update().where(tableUser.c.id == id).values(name=user.name,lastname=user.lastname,url_avatar=user.url_avatar,email=user.email,phone=user.phone))
+        return {"message": "Usuario actualizado exitosamente"}
+    except Exception as e:
+        return {"Error":str(e)}
 
 @userRoute.post('/auth/login')
 def loginUser(data:OAuth2PasswordRequestForm=Depends()):
-    user = load_user(data.username)
-    if user == None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
-    else: 
-        if bcrypt.checkpw(str(data.password).encode('utf-8'),str(user.password).encode('utf-8')):
-            access_token = manager.create_access_token(data={"sub":user.email},expires=timedelta(minutes=60))
-            response = RedirectResponse(url='/home',status_code=status.HTTP_200_OK)
-            response.set_cookie(manager.cookie_name,access_token)
-            usuario = {'id':user.id,'name':user.name,'lastname':user.lastname,'email':user.email,'phone':user.phone,'avatar_url':user.url_avatar}
-            if user.isowner == 1:
-                isowner = conn.execute(select(tableOwner).select_from(tableUser.join(tableOwner, tableOwner.c.user_id == user.id))).first()
-                res = {'token':access_token, "Owner":{"user":usuario,"data_owner":isowner}}
-                return JSONResponse(content=jsonable_encoder(res),status_code=status.HTTP_200_OK)
-            else:
-                res = {'token':access_token, "Client":[usuario]}
-                return JSONResponse(content=jsonable_encoder(res),status_code=status.HTTP_200_OK)
-        else:
+    try:
+        user = load_user(data.username)
+        if user == None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
+        else: 
+            if bcrypt.checkpw(str(data.password).encode('utf-8'),str(user.password).encode('utf-8')):
+                access_token = manager.create_access_token(data={"sub":user.email},expires=timedelta(minutes=60))
+                response = RedirectResponse(url='/home',status_code=status.HTTP_200_OK)
+                response.set_cookie(manager.cookie_name,access_token)
+                usuario = {'id':user.id,'name':user.name,'lastname':user.lastname,'email':user.email,'phone':user.phone,'avatar_url':user.url_avatar}
+                if user.isowner == 1:
+                    isowner = conn.execute(select(tableOwner).select_from(tableUser.join(tableOwner, tableOwner.c.user_id == user.id))).first()
+                    res = {'token':access_token, "Owner":{"user":usuario,"data_owner":isowner}}
+                    return JSONResponse(content=jsonable_encoder(res),status_code=status.HTTP_200_OK)
+                else:
+                    res = {'token':access_token, "Client":[usuario]}
+                    return JSONResponse(content=jsonable_encoder(res),status_code=status.HTTP_200_OK)
+            else:
+                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=e)
 
 @userRoute.post('/auth/register')
 def registerUser(data:Usuario):
-    res = conn.execute(tableUser.select().where(tableUser.c.email == data.email)).first()
-    if res == None:
-        if data.isowner == True:
-            data.password = hashpw(data.password.encode('utf-8'), gensalt())
-            conn.execute(tableUser.insert(), data.dict())
-            result = conn.execute(tableUser.select().where(tableUser.c.email == data.email)).first()
-            conn.execute(tableOwner.insert(), {'user_id':result.id})
-            return 'Dueño creado Correctamente'
+    try:
+        res = conn.execute(tableUser.select().where(tableUser.c.email == data.email)).first()
+        if res == None:
+            if data.isowner == True:
+                data.password = hashpw(data.password.encode('utf-8'), gensalt())
+                conn.execute(tableUser.insert(), data.dict())
+                result = conn.execute(tableUser.select().where(tableUser.c.email == data.email)).first()
+                conn.execute(tableOwner.insert(), {'user_id':result.id})
+                return 'Dueño creado Correctamente'
+            else:
+                data.password = hashpw(data.password.encode('utf-8'), gensalt())
+                conn.execute(tableUser.insert(), data.dict())
+            return 'Usuario Creado Correctamente'
         else:
-            data.password = hashpw(data.password.encode('utf-8'), gensalt())
-            conn.execute(tableUser.insert(), data.dict())
-        return 'Usuario Creado Correctamente'
-    else:
-        return 'El Usuario ya existe'
+            return 'El Usuario ya existe'
+    except Exception as e:
+        return str(e)
 
 @manager.user_loader()
 def load_user(username:str):
