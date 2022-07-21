183c41d094fea6a73309f7c6885a900bf69366e6
Jhonathan Miceli Landa <jhonathanmicelilanda@MacBook-Air-de-Jhonathan.local>
2022-07-20 22:13:16 -0500

    addURI

--- config/db.py
+++ config/db.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-engine = create_engine(url=os.environ.get("DATABASE_URL"), echo=True)
+engine = create_engine(url="mysql+pymysql://ufood:ufood2022@ufoodb.c2ppwslmpouq.us-east-1.rds.amazonaws.com:3306/ufood", echo=True)
 meta = MetaData()
 conn = engine.connect()
