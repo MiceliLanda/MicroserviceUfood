6ea51b332ea483b0afdc037010f94ccbc1b910a4
Jhonathan Miceli Landa <jhonathanmicelilanda@MacBook-Air-de-Jhonathan.local>
2022-07-23 20:48:49 -0500

    addvalues

--- models/shop.py
+++ models/shop.py
@@ -2,3 +2,5 @@ from sqlalchemy import Table
 from config.db import meta, engine
 
 tableShop = Table('shop', meta, autoload=True, autoload_with=engine)
+tableSaucer = Table('saucer', meta, autoload=True, autoload_with=engine)
+tableReview = Table('review', meta, autoload=True, autoload_with=engine)
--- routes/owner.py
+++ routes/owner.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from schemas.shop import Shop, ShopUpdate
 from config.db import conn
 from sqlalchemy import select
-from models.shop import tableShop
+from models.shop import tableShop,tableSaucer,tableReview
 from models.user import tableUser
 from models.owner import tableOwner
 from models.menu import tableMenu
@@ -40,7 +40,7 @@ def createShop(shop:Shop):
             create = conn.execute(tableShop.insert(), shop.dict())
             res = conn.execute(tableMenu.insert(), {'shop_id':str(create.lastrowid)})
             
-        return {"Restaurant":{"idMenu":str(res.lastrowid)}}
+        return {"Restaurant":{"idMenu":str(res.lastrowid),"idShop":str(create.lastrowid)}}
     except Exception as e:
         return {"Error":str(e)}
 
@@ -49,5 +49,17 @@ def updateUser(id: int, shop: ShopUpdate):
     try:
         conn.execute(tableShop.update().where(tableShop.c.id == id).values(name=shop.name,url_shop=shop.url_shop,phone=shop.phone,address=shop.address))
         return {"message": "shop actualizada exitosamente"}
+    except Exception as e:
+        return {"Error":str(e)}
+
+@ownerRoute.delete("/owner/shop/delete/{id}")
+def deleteShop(id_shop: int):
+    try:
+        # conn.execute(tableReview.delete().where(tableReview.c.shop_id == id_shop))
+        # menu_id = conn.execute(select(tableMenu.c.id).where(tableMenu.c.shop_id == id_shop)).first()[0]
+        # conn.execute(tableSaucer.delete().where(tableSaucer.c.menu_id == menu_id))
+        conn.execute(tableMenu.delete().where(tableMenu.c.id == id_shop))
+        conn.execute(tableShop.delete().where(tableShop.c.id == id_shop))
+        return {"message": "Tienda eliminada exitosamente"}
     except Exception as e:
         return {"Error":str(e)}
