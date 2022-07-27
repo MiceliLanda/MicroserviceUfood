58adfda8edab83a1f08eaa6027d6290f79233760
Jhonathan Miceli Landa <jhonathanmicelilanda@MacBook-Air-de-Jhonathan.local>
2022-07-26 22:00:18 -0500

    async functions

--- config/db.py
+++ config/db.py
@@ -1,10 +1,8 @@
 from sqlalchemy import create_engine, MetaData
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
-engine = create_engine(url="mysql+pymysql://ufood:ufood2022@ufoodb.c2ppwslmpouq.us-east-1.rds.amazonaws.com:3306/ufood", echo=True)
-meta = MetaData()
+#autocommit true
+from sqlalchemy.orm import sessionmaker
+engine = create_engine(url="mysql+pymysql://ufood:ufood2022@ufoodb.c2ppwslmpouq.us-east-1.rds.amazonaws.com:3306/ufood")
+meta = MetaData(bind=engine)
 conn = engine.connect()
+Session = sessionmaker(bind=engine,autocommit=True)
+session = Session()
--- routes/user.py
+++ routes/user.py
@@ -7,7 +7,7 @@ from models.owner import tableOwner
 from models.shop import tableShop
 from models.menu import tableMenu
 from schemas.user import Usuario, UsuarioUpdate
-from config.db import conn
+from config.db import session
 from fastapi import Depends, HTTPException, status
 from fastapi.responses import RedirectResponse,JSONResponse
 from fastapi.security import OAuth2PasswordRequestForm,OAuth2PasswordBearer
@@ -28,7 +28,7 @@ async def getUsers():
     try:
     # join table user with table owner
     #query = conn.execute(select(tableUser.c.id,tableUser.c.name,tableUser.c.lastname,tableUser.c.url_avatar,tableUser.c.email,tableUser.c.phone,tableUser.c.isowner,tableOwner).select_from(tableUser.join(tableOwner, tableUser.c.id == tableOwner.c.user_id))).fetchall()
-        user = conn.execute(tableUser.select()).fetchall()
+        user = session.execute(tableUser.select()).all()
         # return {"Owner":query}
         return user
     except Exception as e:
@@ -37,15 +37,15 @@ async def getUsers():
 @userRoute.delete("/auth/delete/{id}")
 async def deleteUser(id: int):
     try:
-        type = conn.execute(tableUser.select().where(tableUser.c.id == id)).first()
+        type = session.execute(tableUser.select().where(tableUser.c.id == id)).first()
         if type == None:
             return {"Error":"No se encontro el usuario"}
         else:
             if type.isowner == 1:
-                conn.execute(tableOwner.delete().where(tableOwner.c.user_id == id))
-                conn.execute(tableUser.delete().where(tableUser.c.id == id))
+                session.execute(tableOwner.delete().where(tableOwner.c.user_id == id))
+                session.execute(tableUser.delete().where(tableUser.c.id == id))
             else:
-                conn.execute(tableUser.delete().where(tableUser.c.id == id))
+                session.execute(tableUser.delete().where(tableUser.c.id == id))
             return {"message": "Usuario eliminado exitosamente"}
     except Exception as e:
         return {"Error":str(e)}
@@ -53,7 +53,7 @@ async def deleteUser(id: int):
 @userRoute.put("/auth/update/{id}")
 async def updateUser(id: int, user: UsuarioUpdate):
     try:
-        conn.execute(tableUser.update().where(tableUser.c.id == id).values(name=user.name,lastname=user.lastname,url_avatar=user.url_avatar,email=user.email,phone=user.phone))
+        session.execute(tableUser.update().where(tableUser.c.id == id).values(name=user.name,lastname=user.lastname,url_avatar=user.url_avatar,email=user.email,phone=user.phone))
         return {"message": "Usuario actualizado exitosamente"}
     except Exception as e:
         return {"Error":str(e)}
@@ -71,8 +71,8 @@ async def loginUser(data:OAuth2PasswordRequestForm=Depends()):
                 response.set_cookie(manager.cookie_name,access_token)
                 usuario = {'id':user.id,'name':user.name,'lastname':user.lastname,'email':user.email,'phone':user.phone,'avatar_url':user.url_avatar,"credits":user.credits}
                 if user.isowner == 1:
-                    isowner = conn.execute(select(tableOwner).select_from(tableUser.join(tableOwner, tableOwner.c.user_id == user.id))).first()
-                    menus = conn.execute(select(tableMenu.c.id,tableShop.c.id).select_from(tableOwner.join(tableShop,tableOwner.c.user_id == tableShop.c.owner_id).join(tableUser,tableOwner.c.user_id == tableUser.c.id).join(tableMenu,tableShop.c.id == tableMenu.c.shop_id)).where(tableOwner.c.user_id == user.id)).fetchall()
+                    isowner = session.execute(select(tableOwner).select_from(tableUser.join(tableOwner, tableOwner.c.user_id == user.id))).first()
+                    menus = session.execute(select(tableMenu.c.id,tableShop.c.id).select_from(tableOwner.join(tableShop,tableOwner.c.user_id == tableShop.c.owner_id).join(tableUser,tableOwner.c.user_id == tableUser.c.id).join(tableMenu,tableShop.c.id == tableMenu.c.shop_id)).where(tableOwner.c.user_id == user.id)).fetchall()
                     id_shops= []
                     for menu in menus:
                         id_shops.append({"id_menu":menu[0],"id_shop":menu[1]})                
@@ -89,17 +89,17 @@ async def loginUser(data:OAuth2PasswordRequestForm=Depends()):
 @userRoute.post('/auth/register')
 async def registerUser(data:Usuario):
     try:
-        res = conn.execute(tableUser.select().where(tableUser.c.email == data.email)).first()
+        res = session.execute(tableUser.select().where(tableUser.c.email == data.email)).first()
         if res == None:
             if data.isowner == True:
                 data.password = hashpw(data.password.encode('utf-8'), gensalt())
-                conn.execute(tableUser.insert(), data.dict())
-                result = conn.execute(tableUser.select().where(tableUser.c.email == data.email)).first()
-                conn.execute(tableOwner.insert(), {'user_id':result.id})
+                session.execute(tableUser.insert(), data.dict())
+                result = session.execute(tableUser.select().where(tableUser.c.email == data.email)).first()
+                session.execute(tableOwner.insert(), {'user_id':result.id})
                 return JSONResponse(content='Dueño creado correctamente',status_code=status.HTTP_200_OK)
             else:
                 data.password = hashpw(data.password.encode('utf-8'), gensalt())
-                conn.execute(tableUser.insert(), data.dict())
+                session.execute(tableUser.insert(), data.dict())
             return JSONResponse(content='Usuario creado correctamente',status_code=status.HTTP_200_OK)
         else:
             return JSONResponse(content='El usuario ya existe',status_code=status.HTTP_400_BAD_REQUEST)
@@ -108,4 +108,4 @@ async def registerUser(data:Usuario):
 
 @manager.user_loader()
 def load_user(username:str):
-    return conn.execute(tableUser.select().where(tableUser.c.email == username)).first()
+    return session.execute(tableUser.select().where(tableUser.c.email == username)).first()
